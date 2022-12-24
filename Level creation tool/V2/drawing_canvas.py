5c83e4fe85d88b67fdb6d77693aada692d673446
Lyle Wong <lylereturns@gmail.com>
2022-12-24 17:40:05 +0000

    Automating current palette tiles system: - Added more palette tiles for testing. - Loads all images inside the palette_tiles directory. - Creates all palette tiles with 14 in each row. - Created new method to create the palette tiles. - Edited existing method to draw the palette tiles. - Re-organising methods so it is easier to find specific methods.

--- Level creation tool/V2/drawing_canvas.py
+++ Level creation tool/V2/drawing_canvas.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, os
 from settings import *
 from pygame import transform as pyt
 from pygame import image as pyi
@@ -46,6 +46,7 @@ class DrawingTiles():
 
         # Create a transparency surface for the grid to be drawn onto
         self.transparency_surface = pygame.Surface((screen_width, screen_height))
+
         # Set the alpha level
         self.transparency_surface.set_alpha(75)
 
@@ -55,22 +56,19 @@ class DrawingTiles():
         # Number to represent what tile the user has selected right now
         self.selected_palette_number = 0
 
-        # Palette tiles group
-        self.palette_tiles_group = pygame.sprite.Group()
-
         # Create palette tiles
-        for i in range(0, 2 + 1):
-            # Create a new palette tile and add it to the palette tiles group
-            palette_tile = PaletteTile(50 + (100 * i), 600, i)
-            self.palette_tiles_group.add(palette_tile)
-
+        self.create_palette_tiles()
+        
         # ----------------------------------------------------------------------------------------
         # Drawing tiles
         self.tile_list = []
+
         # Set the size of the tiles
         self.tile_size = 32 
+
         # Create a tuple with all the images needed for the tiles
-        self.image = ( pyt.scale(pyi.load("V2/graphics/palette_tiles/empty.png"), (32, 32)), pyt.scale(pyi.load("V2/graphics/palette_tiles/1.png"), (32, 32)) , pyt.scale(pyi.load("V2/graphics/palette_tiles/2.png"), (32, 32)) )
+        self.image = self.load_tile_images()
+
         # Create the initial drawing tiles
         self.create_drawing_tiles()
 
@@ -92,6 +90,125 @@ class DrawingTiles():
         self.buttons_group.add(self.reset_tile_map_button)
 
         self.button_clicked_time = pygame.time.get_ticks() # Used to record when a button has been clicked
+
+    # ----------------------------------------------------------------------------------------
+    # Initial set-up methods
+
+    def load_tile_images(self):
+        # Find out the number of images in the following directory 
+        number_of_images = len(os.listdir("V2/graphics/palette_tiles")[:-1])  # [:-1] because one of the images are "empty.png"
+
+        # Create a tuple with all of the images inside of the following directory
+        images_tuple = tuple( pyt.scale(pyi.load(f"V2/graphics/palette_tiles/{i}.png"), (32, 32)) for i in range(0, number_of_images) ) 
+
+        # Return the images tuple
+        return images_tuple
+
+    def create_palette_tiles(self):
+
+        # Create the palette tiles group
+        self.palette_tiles_group = pygame.sprite.Group()
+
+        j = 0 # Keeps track of the current row
+        i = 0 # Keeps track of the current item in the row
+        palette_tile_count = 0 # Keeps track of the current item in the directory
+
+        # While we haven't made all of the palette tiles in the directory
+        while palette_tile_count != len(os.listdir("V2/graphics/palette_tiles")[:-1]): # Number of tiles excluding the "empty" palette tile
+
+            # If we have reached the maximum amount of palette tiles in each row
+            if palette_tile_count % 14 == 0 and palette_tile_count != 0:
+
+                # Go to the next row
+                j += 1
+
+                # Reset i, so that the tile is displayed at the start of the row
+                i = 0
+
+            # Create a new palette tile and add it to the palette tiles group
+            palette_tile = PaletteTile(50 + (100 * i), 500 + (j * 100), palette_tile_count)
+            self.palette_tiles_group.add(palette_tile)
+            
+            # Increment i index and the palette tile count
+            i += 1
+            palette_tile_count += 1
+
+    def create_drawing_tiles(self):
+        # Calculate the number of lines for x and y
+        number_of_lines_x = (screen_width / 2) / 32
+        number_of_lines_y = (screen_height / 2) / 32
+
+        for row in range(0, int(number_of_lines_y)):
+            
+            # Reset the row of items list for every row
+            row_of_items_list = []
+
+            for column in range(0, int(number_of_lines_x)):
+
+                # Create a rect, spacing them out between each other by the tile size
+                rect = pygame.Rect((column * self.tile_size), (row * self.tile_size), self.tile_size, self.tile_size)
+
+                # Create a tile containing the rect and palette number (All palette numbers will be set to 0 by default)
+                tile = [rect, 0] 
+
+                # Add the tile to the row of items list
+                row_of_items_list.append(tile)   
+            
+            # Add the row of items list to the tile list
+            self.tile_list.append(row_of_items_list)
+
+    # ----------------------------------------------------------------------------------------
+    # Interaction methods
+    
+    def extend_drawing_tiles(self):
+
+        # For each row in the tile list
+        for row_count, row in enumerate(self.tile_list):
+
+            # Create a new tile with the palette value, 0, and add it to the end of the row
+            row.append([pygame.Rect( ( (len(row)  * self.tile_size), (row_count * self.tile_size), self.tile_size, self.tile_size)), 0])
+   
+    def shrink_drawing_tiles(self):
+        
+        # If the tile list isn't empty
+        if len(self.tile_list) > 0:
+
+            # For each row in the tile list
+            for row in self.tile_list:
+                
+                if len(row) > 0:
+                    # Remove the last item of the row
+                    row.pop()
+
+    def export_tile_map(self):
+        # List which will hold all the rows of items inside the tile map
+        export_list = []
+
+        # For each row in the tile list
+        for row_count, row in enumerate(self.tile_list):
+
+            # Create a new row of items list
+            row_of_items_list = []
+
+            # For all items in the row
+            for i in range(0, len(row)):
+
+                # Add the palette number of the tile to the row of items list
+                row_of_items_list.append(self.tile_list[row_count][i][1])
+
+            # Add the row of items list to the export list
+            export_list.append(row_of_items_list)
+
+        # Print / Return the tile map
+        print(export_list)
+
+    def reset_tile_map(self):
+        # For each row in the tile list
+        for row in self.tile_list:
+            # For each tile in each row
+            for tile in row:
+                # Set the palette number to 0
+                tile[1] = 0
 
     def draw_grid(self):
 
@@ -222,80 +339,6 @@ class DrawingTiles():
         # Draw the palette tile selected in the middle of the circle
         self.screen.blit(pyt.scale(pyi.load(f"V2/graphics/palette_tiles/{self.selected_palette_number}.png"), (20, 20)), (self.mouse_position[0] - (20 / 2), self.mouse_position[1] - (20 / 2) ) )
 
-    def create_drawing_tiles(self):
-        # Calculate the number of lines for x and y
-        number_of_lines_x = (screen_width / 2) / 32
-        number_of_lines_y = (screen_height / 2) / 32
-
-        for row in range(0, int(number_of_lines_y)):
-            
-            # Reset the row of items list for every row
-            row_of_items_list = []
-
-            for column in range(0, int(number_of_lines_x)):
-
-                # Create a rect, spacing them out between each other by the tile size
-                rect = pygame.Rect((column * self.tile_size), (row * self.tile_size), self.tile_size, self.tile_size)
-
-                # Create a tile containing the rect and palette number (All palette numbers will be set to 0 by default)
-                tile = [rect, 0] 
-
-                # Add the tile to the row of items list
-                row_of_items_list.append(tile)   
-            
-            # Add the row of items list to the tile list
-            self.tile_list.append(row_of_items_list)
-
-    def extend_drawing_tiles(self):
-
-        # For each row in the tile list
-        for row_count, row in enumerate(self.tile_list):
-
-            # Create a new tile with the palette value, 0, and add it to the end of the row
-            row.append([pygame.Rect( ( (len(row)  * self.tile_size), (row_count * self.tile_size), self.tile_size, self.tile_size)), 0])
-   
-    def shrink_drawing_tiles(self):
-        
-        # If the tile list isn't empty
-        if len(self.tile_list) > 0:
-
-            # For each row in the tile list
-            for row in self.tile_list:
-                
-                if len(row) > 0:
-                    # Remove the last item of the row
-                    row.pop()
-
-    def export_tile_map(self):
-        # List which will hold all the rows of items inside the tile map
-        export_list = []
-
-        # For each row in the tile list
-        for row_count, row in enumerate(self.tile_list):
-
-            # Create a new row of items list
-            row_of_items_list = []
-
-            # For all items in the row
-            for i in range(0, len(row)):
-
-                # Add the palette number of the tile to the row of items list
-                row_of_items_list.append(self.tile_list[row_count][i][1])
-
-            # Add the row of items list to the export list
-            export_list.append(row_of_items_list)
-
-        # Print / Return the tile map
-        print(export_list)
-
-    def reset_tile_map(self):
-        # For each row in the tile list
-        for row in self.tile_list:
-            # For each tile in each row
-            for tile in row:
-                # Set the palette number to 0
-                tile[1] = 0
-
     def draw_buttons(self):
 
         # For every button in the buttons group
@@ -305,9 +348,10 @@ class DrawingTiles():
 
     def draw_tiles(self):
         
-        # For every tile in the tile list
+        # For every row in the tile list
         for row in self.tile_list:
 
+            # For each tile in each row
             for tile in row:
 
                 # Draw the appropriate image based on the palette number, at the correct x and y positions from the origin point
@@ -315,10 +359,25 @@ class DrawingTiles():
 
     def draw_palette_tiles(self):
 
+        j = 0 # Keeps track of the current row
+        i = 0 # Keeps track of the current item in the row
+
         # For every palette tile in the palette tiles group
-        for i, palette_tile in enumerate(self.palette_tiles_group):
+        for palette_tile in self.palette_tiles_group:
+
+            # If we have reached the maximum amount of palette tiles in each row
+            if i % 14 == 0 and i != 0:
+
+                # Go to the next row
+                j += 1
+                # Reset i, so that the tile is displayed at the start of the row
+                i = 0
+
             # Draw the palette tile onto the screen
-            palette_tile.draw(50 + (100 * i), 600)
+            palette_tile.draw(50 + (100 * i), 500 + (j * 100))
+
+            # Increment i index
+            i += 1
 
     def run(self):
 
