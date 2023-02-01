2cbd4981b00d1ea92a5497ab77c0a9b3cbac8b36
Lyle Wong <lylereturns@gmail.com>
2023-02-01 01:25:08 +0000

    Adding work on : - Switching between fullscreen mode and windowed mode.

---
+++ WindowedAndFullScreen/main.py
@@ -0,0 +1,105 @@
+import pygame
+from time import perf_counter
+from sys import exit as system_exit
+
+class Main:
+    def __init__(self):
+
+        pygame.init()
+        pygame.display.set_caption("WindowedAndFullscreenSwitching")
+        
+        # Define the screen width and height (this will be the size of the window in windowed mode)
+        # Resolutions with aspect ratios that are not 16:9 will result in black bars
+        self.screen_width = 1600
+        self.screen_height = 900
+
+        """ Set the screen to be full screen and scale the 1600 x 900 resolution to 1920 x 1080 (or the maximum size of the user's monitor)
+        - SCALED scales the 1600 x 900 up to the native resolution of the user.
+        - FULLSCREEN sets the program to full screen
+        """
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height), pygame.SCALED + pygame.FULLSCREEN)
+
+        # Define a scale multiplier (everything will be scaled up by this amount)
+        self.scale_multiplier = 8
+        
+        # Create a scaled surface based on the scale multiplier
+        self.scaled_surface = pygame.Surface((self.screen_width / self.scale_multiplier, self.screen_height/ self.scale_multiplier))
+
+        # Attribute to track if the user is in full-screen or windowed mode
+        self.full_screen = True 
+
+        # Sample image
+        self.sample_image = pygame.image.load("sample_image.png").convert()
+
+        # --------------------------------------------
+        # Time
+        
+        # Record the previous frame that was played
+        self.previous_frame = perf_counter()
+        
+        # Create an object to track time
+        self.clock = pygame.time.Clock()
+        self.chosen_framerate = 60
+
+    def event_loop(self):
+
+        for event in pygame.event.get():
+            
+            # If the user has pressed the exit button
+            if event.type == pygame.QUIT:
+                # Exit the program
+                pygame.quit()
+                system_exit()
+            
+            # If the user has pressed a key
+            if event.type == pygame.KEYDOWN:
+                
+                # If the "f" key is pressed
+                if event.key == pygame.K_f:
+
+                    # If the user is currently in full-screen mode
+                    if self.full_screen == True:
+                        # Set the screen to be in windowed mode
+                        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+                        # Set the full screen attribute to False
+                        self.full_screen = False
+                    
+                    # If the user is currently in windowed mode
+                    elif self.full_screen == False:
+                        # Set the screen to be in full-screen mode
+                        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height), pygame.SCALED + pygame.FULLSCREEN)
+                        # Set the full screen attribute to True
+                        self.full_screen = True        
+    
+    def run(self):
+ 
+        while True:
+            
+            # Limit FPS to 60
+            self.clock.tick(self.chosen_framerate)
+
+            # Calculate delta time 
+            delta_time = perf_counter() - self.previous_frame
+            self.previous_frame = perf_counter()
+
+            # Run the event loop
+            self.event_loop()
+
+            # Fill the scaled surface with a colour
+            self.scaled_surface.fill("BLUE")
+
+            # Blit the sample image onto the screen
+            self.scaled_surface.blit(self.sample_image, (0, 0))
+
+            # Blit the scaled surface onto the main screen
+            self.screen.blit(pygame.transform.scale(self.scaled_surface, (self.screen_width, self.screen_height)), (0, 0))
+            
+            # -------------------------------------
+            # Update display
+            pygame.display.update() 
+            
+
+if __name__ == "__main__":
+    # Instantiate main and run it
+    main = Main()
+    main.run()
